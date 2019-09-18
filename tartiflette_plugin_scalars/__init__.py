99c7c72bfde473be83a65b782ab6e77eb74984b9
AGirard <bouhahah@gmail.com>
2019-09-18 15:50:57 +0200

    feat(scalars): configurable SDL

--- tartiflette_plugin_scalars/__init__.py
+++ tartiflette_plugin_scalars/__init__.py
@@ -1,12 +1,19 @@
 from tartiflette import Scalar
 
-from tartiflette_plugin_scalars.email_address import EmailAddress
-
-_SDL = """
-scalar EmailAddress
-"""
+_SCALAR_TEMPLATE = "scalar {0}"
 
 
-async def bake(schema_name, config):  # pylint: disable=unused-argument
-    Scalar(name="EmailAddress", schema_name=schema_name)(EmailAddress())
-    return _SDL
+async def bake(schema_name, config):
+    scalars = []
+
+    email_address_config = config.get("email_address", {})
+    if email_address_config.get("enabled") is not False:
+        from tartiflette_plugin_scalars.email_address import EmailAddress
+
+        email_address_name = email_address_config.get("name") or "EmailAddress"
+        Scalar(name=email_address_name, schema_name=schema_name)(
+            EmailAddress()
+        )
+        scalars.append(_SCALAR_TEMPLATE.format(email_address_name))
+
+    return "\n".join(scalars)
