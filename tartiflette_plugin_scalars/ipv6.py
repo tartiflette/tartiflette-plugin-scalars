0c28f1217287fc9f35a15321fb4d32b83bff4838
AGirard <bouhahah@gmail.com>
2019-10-01 11:31:31 +0200

    feat(scalars): IPv6

---
+++ tartiflette_plugin_scalars/ipv6.py
@@ -0,0 +1,68 @@
+from ipaddress import IPv4Address, IPv6Address, ip_address
+from typing import Union  # pylint: disable=unused-import
+
+from tartiflette.constants import UNDEFINED_VALUE
+from tartiflette.language.ast import StringValueNode
+
+
+def _parse_ipv6(value: Union[str, IPv6Address]) -> IPv6Address:
+    if isinstance(value, str):
+        value = ip_address(value)
+    if isinstance(value, IPv6Address):
+        return value
+    if isinstance(value, IPv4Address):
+        raise ValueError(f"IPv6 cannot be used to represent IPv4 addresses")
+    raise TypeError(
+        f"IPv6 cannot represent values other than strings and IPv6Address: < {value} >"
+    )
+
+
+class IPv6:
+    """
+    Scalar which handles Internet Protocol version 6 addresses
+    """
+
+    @staticmethod
+    def parse_literal(
+        ast: "ValueNode"
+    ) -> Union[IPv6Address, "UNDEFINED_VALUE"]:
+        """
+        Coerce the input value from an AST node
+        :param ast: ast node to coerce
+        :type ast: ValueNode
+        :return: the value as a IPv6Address object if it can be parsed, UNDEFINED_VALUE otherwise
+        :rtype: Union[IPv6Address, UNDEFINED_VALUE]
+        """
+        if isinstance(ast, StringValueNode):
+            try:
+                return _parse_ipv6(ast.value)
+            except (ValueError, TypeError, OverflowError):
+                return UNDEFINED_VALUE
+        return UNDEFINED_VALUE
+
+    @staticmethod
+    def coerce_input(value: Union[str, int]) -> IPv6Address:
+        """
+        Coerce the input value
+        :param value: the value to coerce
+        :type value: Union[str, int]
+        :return: the value as a IPv6Address object if it can be parsed
+        :rtype: IPv6Address
+        :raises TypeError: if the value isn't a string or int
+        :raises ValueError: if the value isn't convertible to a IPv6Address
+        """
+        return _parse_ipv6(value)
+
+    @staticmethod
+    def coerce_output(value: IPv6Address) -> str:
+        """
+        Coerce the output value
+        :param value: the value to coerce
+        :type value: IPv6Address
+        :return: the value as a IPv6Address object if it can be parsed
+        :raises TypeError: if the value isn't a IPv6Address
+        :rtype: str
+        """
+        if isinstance(value, IPv6Address):
+            return str(value)
+        raise TypeError(f"IPv6 cannot represent value: < {value} >")
