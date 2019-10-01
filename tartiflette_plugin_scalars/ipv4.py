2e35319ee0c482326a7a937a86dceba01fc0692e
AGirard <bouhahah@gmail.com>
2019-10-01 17:50:48 +0200

    refact(scalars): remove useless error checks

--- tartiflette_plugin_scalars/ipv4.py
+++ tartiflette_plugin_scalars/ipv4.py
@@ -36,7 +36,7 @@ class IPv4:
         if isinstance(ast, StringValueNode):
             try:
                 return _parse_ipv4(ast.value)
-            except (ValueError, TypeError, OverflowError):
+            except (ValueError, TypeError):
                 return UNDEFINED_VALUE
         return UNDEFINED_VALUE
 
