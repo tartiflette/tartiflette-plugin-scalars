d70e8f16b4942a3f915584d112874a748b4f316a
AGirard <bouhahah@gmail.com>
2019-10-07 14:38:08 +0200

    refact(tests): more functionnal tests for EmailAddress

--- tests/functional/test_email_address.py
+++ tests/functional/test_email_address.py
@@ -53,3 +53,58 @@ async def test_email_address_nok():
         result["errors"][0]["message"]
         == "Value is not a valid email address: < nope >"
     )
+
+
+@pytest.mark.asyncio
+async def test_email_address_mutation_ok():
+    @Resolver("Mutation.email", schema_name="test_email_address_mutation_ok")
+    async def email_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        email: EmailAddress
+    }
+
+    type Mutation {
+        email(input: EmailAddress): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_email_address_mutation_ok",
+    )
+
+    assert await engine.execute('mutation email { email(input:"alice.girardguittard@dailymotion.com") }') == {
+        "data": {"email":  True}
+    }
+
+
+@pytest.mark.asyncio
+async def test_email_address_mutation_nok():
+    @Resolver("Mutation.email", schema_name="test_email_address_mutation_nok")
+    async def email_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        email: EmailAddress
+    }
+
+    type Mutation {
+        email(input: EmailAddress): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_email_address_mutation_nok",
+    )
+
+    result = await engine.execute('mutation email { email(input:"nok") }')
+    assert result['data'] is None
+    assert len(result['errors']) == 1
+    assert result['errors'][0]['message'] == 'Value nok is not of correct type EmailAddress'
