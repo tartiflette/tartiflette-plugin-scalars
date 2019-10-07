bf1e49cd8405b898d1b773d2c5721ddcaa6b0c62
AGirard <bouhahah@gmail.com>
2019-10-07 18:34:01 +0200

    refact(tests): more functionnal tests for JSONObject

--- tests/functional/test_json_object.py
+++ tests/functional/test_json_object.py
@@ -29,9 +29,9 @@ async def test_json_object_ok():
 
 
 @pytest.mark.asyncio
-async def test_us_curency_nok():
+async def test_json_object_nok():
     @Resolver("Query.jsonObject", schema_name="test_json_object_nok")
-    async def us_currency_resolver(*_args, **_kwargs):
+    async def json_object_resolver(*_args, **_kwargs):
         return "nok"
 
     sdl = """
@@ -53,3 +53,58 @@ async def test_us_curency_nok():
         result["errors"][0]["message"]
         == "JSONObject cannot represent value: < nok >"
     )
+
+
+@pytest.mark.asyncio
+async def test_json_object_mutation_ok():
+    @Resolver("Mutation.jsonObject", schema_name="test_json_object_mutation_ok")
+    async def json_object_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        jsonObject: JSONObject
+    }
+
+    type Mutation {
+        jsonObject(input: JSONObject): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_json_object_mutation_ok",
+    )
+
+    assert await engine.execute("""mutation jsonObject { jsonObject(input:"{\\\"ok\\\": 2}") }""") == {
+        "data": {"jsonObject":  True}
+    }
+
+
+@pytest.mark.asyncio
+async def test_json_object_mutation_nok():
+    @Resolver("Mutation.jsonObject", schema_name="test_json_object_mutation_nok")
+    async def json_object_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        jsonObject: JSONObject
+    }
+
+    type Mutation {
+        jsonObject(input: JSONObject): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_json_object_mutation_nok",
+    )
+
+    result = await engine.execute('mutation jsonObject { jsonObject(input:"nok") }')
+    assert result['data'] is None
+    assert len(result['errors']) == 1
+    assert result['errors'][0]['message'] == 'Value nok is not of correct type JSONObject'
