c9e71421278ff1d14a1ab0056e2888cc6e58ecab
AGirard <bouhahah@gmail.com>
2019-10-07 17:36:24 +0200

    refact(tests): more functionnal tests for RGB

--- tests/functional/test_rgb.py
+++ tests/functional/test_rgb.py
@@ -52,3 +52,58 @@ async def test_rgb_nok():
     assert (
         result["errors"][0]["message"] == "Value is not a valid RGB: < nope >"
     )
+
+
+@pytest.mark.asyncio
+async def test_rgb_mutation_ok():
+    @Resolver("Mutation.rgb", schema_name="test_rgb_mutation_ok")
+    async def rgb_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        rgb: RGB
+    }
+
+    type Mutation {
+        rgb(input: RGB): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_rgb_mutation_ok",
+    )
+
+    assert await engine.execute('mutation rgb { rgb(input:"rgb(100%, 0%, 60%)") }') == {
+        "data": {"rgb":  True}
+    }
+
+
+@pytest.mark.asyncio
+async def test_rgb_mutation_nok():
+    @Resolver("Mutation.rgb", schema_name="test_rgb_mutation_nok")
+    async def rgb_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        rgb: RGB
+    }
+
+    type Mutation {
+        rgb(input: RGB): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_rgb_mutation_nok",
+    )
+
+    result = await engine.execute('mutation rgb { rgb(input:"nok") }')
+    assert result['data'] is None
+    assert len(result['errors']) == 1
+    assert result['errors'][0]['message'] == 'Value nok is not of correct type RGB'
