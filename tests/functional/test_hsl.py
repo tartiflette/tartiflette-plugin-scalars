bba6c0cf8f9a63c871ca7c6694220fbad04b6143
AGirard <bouhahah@gmail.com>
2019-10-07 17:21:21 +0200

    refact(tests): more functionnal tests for HSL

--- tests/functional/test_hsl.py
+++ tests/functional/test_hsl.py
@@ -52,3 +52,58 @@ async def test_hsl_nok():
     assert (
         result["errors"][0]["message"] == "Value is not a valid HSL: < nope >"
     )
+
+
+@pytest.mark.asyncio
+async def test_hsl_mutation_ok():
+    @Resolver("Mutation.hsl", schema_name="test_hsl_mutation_ok")
+    async def hsl_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        hsl: HSL
+    }
+
+    type Mutation {
+        hsl(input: HSL): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_hsl_mutation_ok",
+    )
+
+    assert await engine.execute('mutation hsl { hsl(input:"hsl(270, 60%, 50%)") }') == {
+        "data": {"hsl":  True}
+    }
+
+
+@pytest.mark.asyncio
+async def test_hsl_mutation_nok():
+    @Resolver("Mutation.hsl", schema_name="test_hsl_mutation_nok")
+    async def hsl_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        hsl: HSL
+    }
+
+    type Mutation {
+        hsl(input: HSL): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_hsl_mutation_nok",
+    )
+
+    result = await engine.execute('mutation hsl { hsl(input:"nok") }')
+    assert result['data'] is None
+    assert len(result['errors']) == 1
+    assert result['errors'][0]['message'] == 'Value nok is not of correct type HSL'
