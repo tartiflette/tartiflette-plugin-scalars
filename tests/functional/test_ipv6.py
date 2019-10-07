971085ac882837c24a08bfe6ab93efb9afe3a61e
AGirard <bouhahah@gmail.com>
2019-10-07 18:04:49 +0200

    refact(tests): more functionnal tests for IPv6

--- tests/functional/test_ipv6.py
+++ tests/functional/test_ipv6.py
@@ -54,3 +54,58 @@ async def test_ipv6_ok_nok():
         result["errors"][0]["message"]
         == "IPv6 cannot represent value: < nok >"
     )
+
+
+@pytest.mark.asyncio
+async def test_ipv6_mutation_ok():
+    @Resolver("Mutation.ipv6", schema_name="test_ipv6_mutation_ok")
+    async def ipv6_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        ipv6: IPv6
+    }
+
+    type Mutation {
+        ipv6(input: IPv6): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_ipv6_mutation_ok",
+    )
+
+    assert await engine.execute('mutation ipv6 { ipv6(input:"2001:0db8:0000:0000:0000:8a2e:0370:7334") }') == {
+        "data": {"ipv6":  True}
+    }
+
+
+@pytest.mark.asyncio
+async def test_ipv6_mutation_nok():
+    @Resolver("Mutation.ipv6", schema_name="test_ipv6_mutation_nok")
+    async def ipv6_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        ipv6: IPv6
+    }
+
+    type Mutation {
+        ipv6(input: IPv6): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_ipv6_mutation_nok",
+    )
+
+    result = await engine.execute('mutation ipv6 { ipv6(input:"nok") }')
+    assert result['data'] is None
+    assert len(result['errors']) == 1
+    assert result['errors'][0]['message'] == 'Value nok is not of correct type IPv6'
