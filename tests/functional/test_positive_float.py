770b49afd9d5e4a5edc0732769c9bebe1246e904
AGirard <bouhahah@gmail.com>
2019-10-07 15:54:28 +0200

    refact(tests): more functionnal tests for PositiveFloat

--- tests/functional/test_positive_float.py
+++ tests/functional/test_positive_float.py
@@ -53,3 +53,58 @@ async def test_positive_float_nok():
         result["errors"][0]["message"]
         == "could not convert string to float: 'nope'"
     )
+
+
+@pytest.mark.asyncio
+async def test__positive_float_nok_mutation_ok():
+    @Resolver("Mutation.positiveFloat", schema_name="test_positive_float_mutation_ok")
+    async def positive_float_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        positiveFloat: PositiveFloat
+    }
+
+    type Mutation {
+        positiveFloat(input: PositiveFloat): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_positive_float_mutation_ok",
+    )
+
+    assert await engine.execute('mutation positiveFloat { positiveFloat(input:100) }') == {
+        "data": {"positiveFloat":  True}
+    }
+
+
+@pytest.mark.asyncio
+async def test_positive_float_mutation_nok():
+    @Resolver("Mutation.positiveFloat", schema_name="test_positive_float_mutation_nok")
+    async def positive_float_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        positiveFloat: PositiveFloat
+    }
+
+    type Mutation {
+        positiveFloat(input: PositiveFloat): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_positive_float_mutation_nok",
+    )
+
+    result = await engine.execute('mutation positiveFloat { positiveFloat(input:-100) }')
+    assert result['data'] is None
+    assert len(result['errors']) == 1
+    assert result['errors'][0]['message'] == 'Value -100 is not of correct type PositiveFloat'
