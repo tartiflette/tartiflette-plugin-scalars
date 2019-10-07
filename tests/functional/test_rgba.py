70ab2b0c8094679d6f42b2c982720492038a30ee
AGirard <bouhahah@gmail.com>
2019-10-07 17:41:07 +0200

    refact(tests): more functionnal tests for RGBA

--- tests/functional/test_rgba.py
+++ tests/functional/test_rgba.py
@@ -52,3 +52,57 @@ async def test_rgba_nok():
     assert (
         result["errors"][0]["message"] == "Value is not a valid RGBA: < nope >"
     )
+
+@pytest.mark.asyncio
+async def test_rgba_mutation_ok():
+    @Resolver("Mutation.rgba", schema_name="test_rgba_mutation_ok")
+    async def rgba_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        rgba: RGBA
+    }
+
+    type Mutation {
+        rgba(input: RGBA): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_rgba_mutation_ok",
+    )
+
+    assert await engine.execute('mutation rgba { rgba(input:"rgba(100%, 0%, 60%, .5)") }') == {
+        "data": {"rgba":  True}
+    }
+
+
+@pytest.mark.asyncio
+async def test_rgba_mutation_nok():
+    @Resolver("Mutation.rgba", schema_name="test_rgba_mutation_nok")
+    async def rgba_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        rgba: RGBA
+    }
+
+    type Mutation {
+        rgba(input: RGBA): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_rgba_mutation_nok",
+    )
+
+    result = await engine.execute('mutation rgba { rgba(input:"nok") }')
+    assert result['data'] is None
+    assert len(result['errors']) == 1
+    assert result['errors'][0]['message'] == 'Value nok is not of correct type RGBA'
