2cddb8994ae75df68757fe138343fe38f2e5eb48
AGirard <bouhahah@gmail.com>
2019-10-07 17:16:06 +0200

    refact(tests): more functionnal tests for GUID

--- tests/functional/test_guid.py
+++ tests/functional/test_guid.py
@@ -52,3 +52,58 @@ async def test_guid_nok():
     assert (
         result["errors"][0]["message"] == "Value is not a valid GUID: < nope >"
     )
+
+
+@pytest.mark.asyncio
+async def test_guid_mutation_ok():
+    @Resolver("Mutation.guid", schema_name="test_guid_mutation_ok")
+    async def guid_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        guid: GUID
+    }
+
+    type Mutation {
+        guid(input: GUID): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_guid_mutation_ok",
+    )
+
+    assert await engine.execute('mutation guid { guid(input:"5591b4e9-c747-45ae-8abf-d9cd1a17081e") }') == {
+        "data": {"guid":  True}
+    }
+
+
+@pytest.mark.asyncio
+async def test_guid_mutation_nok():
+    @Resolver("Mutation.guid", schema_name="test_guid_mutation_nok")
+    async def guid_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        guid: GUID
+    }
+
+    type Mutation {
+        guid(input: GUID): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_guid_mutation_nok",
+    )
+
+    result = await engine.execute('mutation guid { guid(input:"nok") }')
+    assert result['data'] is None
+    assert len(result['errors']) == 1
+    assert result['errors'][0]['message'] == 'Value nok is not of correct type GUID'
