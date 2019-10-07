1682aaa36e65fadb57669a1301722f04fdc3a98f
AGirard <bouhahah@gmail.com>
2019-10-07 16:50:09 +0200

    refact(tests): more functionnal tests for URL

--- tests/functional/test_url.py
+++ tests/functional/test_url.py
@@ -64,3 +64,58 @@ async def test_url_nok():
         result["errors"][0]["message"]
         == "URL cannot represent value: < ParseResult(scheme='', netloc='', path='', params='', query='', fragment='') >"
     )
+
+
+@pytest.mark.asyncio
+async def test_url_mutation_ok():
+    @Resolver("Mutation.url", schema_name="test_url_mutation_ok")
+    async def url_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        url: URL
+    }
+
+    type Mutation {
+        url(input: URL): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_url_mutation_ok",
+    )
+
+    assert await engine.execute('mutation url { url(input:"https://www.dailymotion.com/") }') == {
+        "data": {"url":  True}
+    }
+
+
+@pytest.mark.asyncio
+async def test_url_mutation_nok():
+    @Resolver("Mutation.url", schema_name="test_url_mutation_nok")
+    async def url_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        url: URL
+    }
+
+    type Mutation {
+        url(input: URL): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_url_mutation_nok",
+    )
+
+    result = await engine.execute('mutation url { url(input:"nok") }')
+    assert result['data'] is None
+    assert len(result['errors']) == 1
+    assert result['errors'][0]['message'] == 'Value nok is not of correct type URL'
