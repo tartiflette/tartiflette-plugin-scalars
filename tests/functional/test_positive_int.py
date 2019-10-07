0e9c6c413fdb59545f331acd8cc50ab9abab2cc6
AGirard <bouhahah@gmail.com>
2019-10-07 16:00:50 +0200

    refact(tests): more functionnal tests for PositiveInt

--- tests/functional/test_positive_int.py
+++ tests/functional/test_positive_int.py
@@ -53,3 +53,58 @@ async def test_positive_int_nok():
         result["errors"][0]["message"]
         == "invalid literal for int() with base 10: 'nope'"
     )
+
+
+@pytest.mark.asyncio
+async def test_positive_int_mutation_ok():
+    @Resolver("Mutation.positiveInt", schema_name="test_positive_int_mutation_ok")
+    async def positive_int_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        positiveInt: PositiveInt
+    }
+
+    type Mutation {
+        positiveInt(input: PositiveInt): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_positive_int_mutation_ok",
+    )
+
+    assert await engine.execute('mutation positiveInt { positiveInt(input:100) }') == {
+        "data": {"positiveInt":  True}
+    }
+
+
+@pytest.mark.asyncio
+async def test_positive_int_mutation_nok():
+    @Resolver("Mutation.positiveInt", schema_name="test_positive_int_mutation_nok")
+    async def positive_int_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        positiveInt: PositiveInt
+    }
+
+    type Mutation {
+        positiveInt(input: PositiveInt): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_positive_int_mutation_nok",
+    )
+
+    result = await engine.execute('mutation positiveInt { positiveInt(input:-100) }')
+    assert result['data'] is None
+    assert len(result['errors']) == 1
+    assert result['errors'][0]['message'] == 'Value -100 is not of correct type PositiveInt'
