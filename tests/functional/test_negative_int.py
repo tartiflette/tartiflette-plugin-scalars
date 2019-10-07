d7189466128501bef8427ce092530ee1e9186953
AGirard <bouhahah@gmail.com>
2019-10-07 15:27:34 +0200

    refact(tests): more functionnal tests for NegativeInt

--- tests/functional/test_negative_int.py
+++ tests/functional/test_negative_int.py
@@ -53,3 +53,58 @@ async def test_negative_int_nok():
         result["errors"][0]["message"]
         == "invalid literal for int() with base 10: 'nope'"
     )
+
+
+@pytest.mark.asyncio
+async def test_negative_int_nok_mutation_ok():
+    @Resolver("Mutation.negativeInt", schema_name="test_negative_int_mutation_ok")
+    async def negative_int_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        negativeInt: NegativeInt
+    }
+
+    type Mutation {
+        negativeInt(input: NegativeInt): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_negative_int_mutation_ok",
+    )
+
+    assert await engine.execute('mutation negativeInt { negativeInt(input:-100) }') == {
+        "data": {"negativeInt":  True}
+    }
+
+
+@pytest.mark.asyncio
+async def test_negative_int_mutation_nok():
+    @Resolver("Mutation.negativeInt", schema_name="test_negative_int_mutation_nok")
+    async def negativeInt_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        negativeInt: NegativeInt
+    }
+
+    type Mutation {
+        negativeInt(input: NegativeInt): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_negative_int_mutation_nok",
+    )
+
+    result = await engine.execute('mutation negativeInt { negativeInt(input:100) }')
+    assert result['data'] is None
+    assert len(result['errors']) == 1
+    assert result['errors'][0]['message'] == 'Value 100 is not of correct type NegativeInt'
