6a5ed1bda2df6018ba28176ca000bd45dde51a65
AGirard <bouhahah@gmail.com>
2019-10-07 18:17:01 +0200

    refact(tests): more functionnal tests for USCurrency

--- tests/functional/test_us_currency.py
+++ tests/functional/test_us_currency.py
@@ -53,3 +53,58 @@ async def test_us_currency_nok():
         result["errors"][0]["message"]
         == "USCurrency cannot represent value: < nok >"
     )
+
+
+@pytest.mark.asyncio
+async def test_us_currency_mutation_ok():
+    @Resolver("Mutation.usCurrency", schema_name="test_us_currency_mutation_ok")
+    async def us_currency_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        usCurrency: USCurrency
+    }
+
+    type Mutation {
+        usCurrency(input: USCurrency): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_us_currency_mutation_ok",
+    )
+
+    assert await engine.execute('mutation usCurrency { usCurrency(input:"75017") }') == {
+        "data": {"usCurrency":  True}
+    }
+
+
+@pytest.mark.asyncio
+async def test_us_currency_mutation_nok():
+    @Resolver("Mutation.usCurrency", schema_name="test_us_currency_mutation_nok")
+    async def us_currency_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        usCurrency: USCurrency
+    }
+
+    type Mutation {
+        usCurrency(input: USCurrency): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_us_currency_mutation_nok",
+    )
+
+    result = await engine.execute('mutation usCurrency { usCurrency(input:"nok") }')
+    assert result['data'] is None
+    assert len(result['errors']) == 1
+    assert result['errors'][0]['message'] == 'Value nok is not of correct type USCurrency'
