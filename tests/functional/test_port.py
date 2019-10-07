85a8ce234f59d7b13435e474da50138c347c9691
AGirard <bouhahah@gmail.com>
2019-10-07 18:15:10 +0200

    refact(tests): more functionnal tests for Port

--- tests/functional/test_port.py
+++ tests/functional/test_port.py
@@ -53,3 +53,58 @@ async def test_port_nok():
         result["errors"][0]["message"]
         == "Port cannot represent values below or equal to 0: < -2 >"
     )
+
+
+@pytest.mark.asyncio
+async def test_port_mutation_ok():
+    @Resolver("Mutation.port", schema_name="test_port_mutation_ok")
+    async def port_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        port: Port
+    }
+
+    type Mutation {
+        port(input: Port): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_port_mutation_ok",
+    )
+
+    assert await engine.execute('mutation port { port(input:80) }') == {
+        "data": {"port":  True}
+    }
+
+
+@pytest.mark.asyncio
+async def test_port_mutation_nok():
+    @Resolver("Mutation.port", schema_name="test_port_mutation_nok")
+    async def port_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        port: Port
+    }
+
+    type Mutation {
+        port(input: Port): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_port_mutation_nok",
+    )
+
+    result = await engine.execute('mutation port { port(input:999999) }')
+    assert result['data'] is None
+    assert len(result['errors']) == 1
+    assert result['errors'][0]['message'] == 'Value 999999 is not of correct type Port'
