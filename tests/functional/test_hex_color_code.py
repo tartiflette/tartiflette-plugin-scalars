5d0955af0f135dc57936a55d9b3701d0175f115f
AGirard <bouhahah@gmail.com>
2019-10-07 17:19:25 +0200

    refact(tests): more functionnal tests for HexColorCode

--- tests/functional/test_hex_color_code.py
+++ tests/functional/test_hex_color_code.py
@@ -53,3 +53,58 @@ async def test_hex_color_code_nok():
         result["errors"][0]["message"]
         == "Value is not a valid HexColorCode: < nope >"
     )
+
+
+@pytest.mark.asyncio
+async def test_hex_color_code_mutation_ok():
+    @Resolver("Mutation.hexColorCode", schema_name="test_hex_color_code_mutation_ok")
+    async def hex_color_code_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        hexColorCode: HexColorCode
+    }
+
+    type Mutation {
+        hexColorCode(input: HexColorCode): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_hex_color_code_mutation_ok",
+    )
+
+    assert await engine.execute('mutation hexColorCode { hexColorCode(input:"#ffffff") }') == {
+        "data": {"hexColorCode":  True}
+    }
+
+
+@pytest.mark.asyncio
+async def test_hex_color_code_mutation_nok():
+    @Resolver("Mutation.hexColorCode", schema_name="test_hex_color_code_mutation_nok")
+    async def hex_color_code_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        hexColorCode: HexColorCode
+    }
+
+    type Mutation {
+        hexColorCode(input: HexColorCode): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_hex_color_code_mutation_nok",
+    )
+
+    result = await engine.execute('mutation hexColorCode { hexColorCode(input:"nok") }')
+    assert result['data'] is None
+    assert len(result['errors']) == 1
+    assert result['errors'][0]['message'] == 'Value nok is not of correct type HexColorCode'
