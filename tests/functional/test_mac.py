1d953c3520b19bc07a557c6b5dbf6927ee5ee2d9
AGirard <bouhahah@gmail.com>
2019-10-07 18:13:35 +0200

    refact(tests): more functionnal tests for MAC

--- tests/functional/test_mac.py
+++ tests/functional/test_mac.py
@@ -53,3 +53,58 @@ async def test_mac_nok():
         result["errors"][0]["message"]
         == "Value is not a valid MAC address: < nope >"
     )
+
+
+@pytest.mark.asyncio
+async def test_mac_mutation_ok():
+    @Resolver("Mutation.mac", schema_name="test_mac_mutation_ok")
+    async def mac_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        mac: MAC
+    }
+
+    type Mutation {
+        mac(input: MAC): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_mac_mutation_ok",
+    )
+
+    assert await engine.execute('mutation mac { mac(input:"00:0a:95:9d:68:16") }') == {
+        "data": {"mac":  True}
+    }
+
+
+@pytest.mark.asyncio
+async def test_mac_mutation_nok():
+    @Resolver("Mutation.mac", schema_name="test_mac_mutation_nok")
+    async def mac_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        mac: MAC
+    }
+
+    type Mutation {
+        mac(input: MAC): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_mac_mutation_nok",
+    )
+
+    result = await engine.execute('mutation mac { mac(input:"nok") }')
+    assert result['data'] is None
+    assert len(result['errors']) == 1
+    assert result['errors'][0]['message'] == 'Value nok is not of correct type MAC'
