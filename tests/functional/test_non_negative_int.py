78205fbe1b53562e992aa21f9833e8baf1a6ddcc
AGirard <bouhahah@gmail.com>
2019-10-07 15:34:57 +0200

    refact(tests): more functionnal tests for NonNegativeInt

--- tests/functional/test_non_negative_int.py
+++ tests/functional/test_non_negative_int.py
@@ -53,3 +53,58 @@ async def test_negative_int_nok():
         result["errors"][0]["message"]
         == "NonNegativeInt cannot represent values below 0: < -999 >"
     )
+
+
+@pytest.mark.asyncio
+async def test_non_negative_int_nok_mutation_ok():
+    @Resolver("Mutation.nonNegativeInt", schema_name="test_non_negative_int_mutation_ok")
+    async def non_negative_int_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        nonNegativeInt: NonNegativeInt
+    }
+
+    type Mutation {
+        nonNegativeInt(input: NonNegativeInt): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_non_negative_int_mutation_ok",
+    )
+
+    assert await engine.execute('mutation nonNegativeInt { nonNegativeInt(input:100) }') == {
+        "data": {"nonNegativeInt":  True}
+    }
+
+
+@pytest.mark.asyncio
+async def test_non_negative_int_mutation_nok():
+    @Resolver("Mutation.nonNegativeInt", schema_name="test_non_negative_int_mutation_nok")
+    async def nonNegativeInt_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        nonNegativeInt: NonNegativeInt
+    }
+
+    type Mutation {
+        nonNegativeInt(input: NonNegativeInt): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_non_negative_int_mutation_nok",
+    )
+
+    result = await engine.execute('mutation nonNegativeInt { nonNegativeInt(input:-100) }')
+    assert result['data'] is None
+    assert len(result['errors']) == 1
+    assert result['errors'][0]['message'] == 'Value -100 is not of correct type NonNegativeInt'
