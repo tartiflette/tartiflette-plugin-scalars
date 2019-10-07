fdf09fdf882c67dcb9255b3129c0f28f3533c249
AGirard <bouhahah@gmail.com>
2019-10-07 18:02:19 +0200

    refact(tests): more functionnal tests for IPv4

--- tests/functional/test_ipv4.py
+++ tests/functional/test_ipv4.py
@@ -54,3 +54,58 @@ async def test_ipv4_ok_nok():
         result["errors"][0]["message"]
         == "IPv4 cannot represent value: < nok >"
     )
+
+
+@pytest.mark.asyncio
+async def test_ipv4_mutation_ok():
+    @Resolver("Mutation.ipv4", schema_name="test_ipv4_mutation_ok")
+    async def ipv4_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        ipv4: IPv4
+    }
+
+    type Mutation {
+        ipv4(input: IPv4): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_ipv4_mutation_ok",
+    )
+
+    assert await engine.execute('mutation ipv4 { ipv4(input:"127.0.0.1") }') == {
+        "data": {"ipv4":  True}
+    }
+
+
+@pytest.mark.asyncio
+async def test_ipv4_mutation_nok():
+    @Resolver("Mutation.ipv4", schema_name="test_ipv4_mutation_nok")
+    async def ipv4_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        ipv4: IPv4
+    }
+
+    type Mutation {
+        ipv4(input: IPv4): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_ipv4_mutation_nok",
+    )
+
+    result = await engine.execute('mutation ipv4 { ipv4(input:"nok") }')
+    assert result['data'] is None
+    assert len(result['errors']) == 1
+    assert result['errors'][0]['message'] == 'Value nok is not of correct type IPv4'
