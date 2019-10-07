7b8fa6c0ea5c333427de0758f5c70d8dbfe3d4de
AGirard <bouhahah@gmail.com>
2019-10-07 15:50:28 +0200

    refact(tests): more functionnal tests for NonPositiveInt

--- tests/functional/test_non_positive_int.py
+++ tests/functional/test_non_positive_int.py
@@ -53,3 +53,58 @@ async def test_non_positive_int_nok():
         result["errors"][0]["message"]
         == "invalid literal for int() with base 10: 'nope'"
     )
+
+
+@pytest.mark.asyncio
+async def test_non_positive_int_nok_mutation_ok():
+    @Resolver("Mutation.nonPositiveInt", schema_name="test_non_positive_int_mutation_ok")
+    async def non_positive_int_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        nonPositiveInt: NonPositiveInt
+    }
+
+    type Mutation {
+        nonPositiveInt(input: NonPositiveInt): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_non_positive_int_mutation_ok",
+    )
+
+    assert await engine.execute('mutation nonPositiveInt { nonPositiveInt(input:-100) }') == {
+        "data": {"nonPositiveInt":  True}
+    }
+
+
+@pytest.mark.asyncio
+async def test_non_positive_int_mutation_nok():
+    @Resolver("Mutation.nonPositiveInt", schema_name="test_non_positive_int_mutation_nok")
+    async def non_positive_int_resolver(*_args, **_kwargs):
+        return True
+
+    sdl = """
+    type Query {
+        nonPositiveInt: NonPositiveInt
+    }
+
+    type Mutation {
+        nonPositiveInt(input: NonPositiveInt): Boolean
+    }
+    """
+
+    engine = await create_engine(
+        sdl=sdl,
+        modules=[{"name": "tartiflette_plugin_scalars", "config": {}}],
+        schema_name="test_non_positive_int_mutation_nok",
+    )
+
+    result = await engine.execute('mutation nonPositiveInt { nonPositiveInt(input:100) }')
+    assert result['data'] is None
+    assert len(result['errors']) == 1
+    assert result['errors'][0]['message'] == 'Value 100 is not of correct type NonPositiveInt'
