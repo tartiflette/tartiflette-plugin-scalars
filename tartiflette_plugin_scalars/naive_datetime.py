378c82a6d22a251eab8f4e617e5ec7a60837c242
joe <joelit@me.com>
2020-08-27 14:33:10 -0500

    scalar name in error

--- tartiflette_plugin_scalars/naive_datetime.py
+++ tartiflette_plugin_scalars/naive_datetime.py
@@ -70,7 +70,7 @@ class NaiveDateTime:
             try:
                 datetime.fromisoformat(value)
             except ValueError:
-                raise ValueError(f"DateTime cannot represent value: < {value} >")
+                raise ValueError(f"NaiveDateTime cannot represent value: < {value} >")
             else:
                 return value
         raise TypeError(f"NaiveDateTime cannot represent value: < {value} >")
